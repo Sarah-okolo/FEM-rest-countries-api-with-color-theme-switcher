f639b2afe6578a846eb2ed65f7223a19fde7386c
Sarah-okolo <okolosarah402@gmail.com>
2024-09-17 00:11:32 +0100

    Created boilerplate styles and rearranged project files

---
+++ src/components/Search-and-filter.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function SearchAndFilter() {
+  return (
+    <>
+      <div className="search-and-filter">
+      
+      </div>
+    </>
+  )
+}
+
+export default SearchAndFilter
