794b0f744acfd41103fb1fe68833cb81b209c272
Sarah-okolo <okolosarah402@gmail.com>
2024-10-02 11:04:01 +0100

    Implemented light dark theme for site

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,46 @@
-import {useRef} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import '/src/index.scss'
 
 function Header() {
   const themeTextRef = useRef(null);
   const themeIconRef = useRef(null);
-  
+
+  // Check if user has a saved theme preference, if not, set light mode as default. // true - light, false - dark
+  const [isLightMode, setIsLightMode] = useState(() => {
+    if (localStorage.getItem('userThemeChoice') === null) {
+      localStorage.setItem('userThemeChoice', true);
+      return true;
+    }
+    else {
+      return localStorage.getItem('userThemeChoice') === 'true' ? true : false;
+    }
+  });
+
+  // Save user's theme preference to local storage
+  useEffect(() => {
+    localStorage.setItem('userThemeChoice', isLightMode);
+  }, [isLightMode])
+
+  // On page load, set site theme based on user's currently saved theme.
+  useEffect(() => {
+    if (isLightMode) {
+      themeIconRef.current.name = "moon-outline";
+      themeTextRef.current.textContent = "Dark Mode";
+      document.documentElement.classList.remove('darkMode');
+    }
+    else {
+      themeIconRef.current.name = "sunny-outline";
+      themeTextRef.current.textContent = "Light Mode";
+      document.documentElement.classList.add('darkMode');
+    }
+  }, []);
+
+  // Handle theme switch
   const toggleTheme = () => {
     themeIconRef.current.name = themeIconRef.current.name === "moon-outline" ? "sunny-outline" : "moon-outline";
     themeTextRef.current.textContent = themeTextRef.current.textContent === "Dark Mode" ? "Light Mode" : "Dark Mode";
-
-    document.body.classList.toggle('darkMode')
+    document.documentElement.classList.toggle('darkMode');
+    setIsLightMode(preVal => !preVal);
   }
 
 
